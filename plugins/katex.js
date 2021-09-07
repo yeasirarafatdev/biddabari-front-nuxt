1421822f3aaa508a2b4fb4ee33755745b328979c
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2021-09-07 18:39:33 +0600

    maths equation fixed

--- plugins/katex.js
+++ plugins/katex.js
@@ -6,5 +6,11 @@ import 'katex/dist/katex.min.css';
 Vue.use(VueKatex, {
     globalOptions: {
         // ... Define globally applied KaTeX options here
+        delimiters: [
+            {left: "$$", right: "$$", display: true},
+            {left: "$", right: "$", display: false},
+            {left: "\\(", right: "\\)", display: false},
+            {left: "\\[", right: "\\]", display: true}
+        ]
     }
 });
