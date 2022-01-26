f411995e4677548d081445099d170f2f3d33b776
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2022-01-26 16:02:06 +0600

    Exam Components merged for mobile and web

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 
+/*const moment = require('moment-timezone')
+moment().tz('Asia/Dhaka').format()*/
 
 Vue.filter('truncate', function(value, length) {
     if (!value) return ''
     value = value.toString()
-    if (value.length > length) {
-        return value.substring(0, length) + '...'
-    } else {
-        return value
-    }
+    if (value.length > length) return value.substring(0, length) + '...'
+    else return value
 })
+
 Vue.filter('capitalize', function(value) {
-    if (!value) {
-        return ''
-    }
+    if (!value) return ''
     value = value.toString()
     return value.charAt(0).toUpperCase() + value.slice(1)
 })
